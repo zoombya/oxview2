6d89268c3f9b344f450f5aa39a571031c327caa0
zoombya <progolab@gmail.com>
2023-06-11 22:15:52 -0700

    nucleosides added

--- src/utils/document.js
+++ src/utils/document.js
@@ -5,16 +5,8 @@ import * as THREE from '/node_modules/three/build/three.module.js';
 
 class oxDocument extends THREE.Group{
 
-    // Default colors for the backbones
-    oxStrandColors = [
-        new THREE.Color(0xfdd291), //light yellow
-        
-        new THREE.Color(0xffb322), //goldenrod
 
-        new THREE.Color(0x437092), //dark blue
-
-        new THREE.Color(0x6ea4cc), //light blue
-        ];
+    
 
     constructor(){
         super();
@@ -44,18 +36,31 @@ class oxDocument extends THREE.Group{
 
         // load the configuration file
         // create a web worker to parse the configuration file
-        let particle_count, stand_count, strand_ids;
+        let particle_count, stand_count, strand_ids, nuc_ids;
         const pworker = new Worker('/src/utils/file_worker.js');
        
         pworker.onmessage = event => {
             console.log("got message from worker");
             const startTime = performance.now();
             const { coordinates } = event.data;
-            // const geometry = new THREE.SphereGeometry(.3, 10, 10);
-            //const geometry = new THREE.SphereGeometry(.2,10,10);
-            const geometry = new THREE.SphereGeometry(.4,10,10);
-            const spheres = new THREE.InstancedMesh(geometry, material, coordinates.length);
+            const bb_geometry = new THREE.SphereGeometry(.2,10,10);
+            const bbSpheres = new THREE.InstancedMesh(bb_geometry, material, coordinates.length);
+            
+            const nucGeometry = new THREE.SphereGeometry(.3,10,10);
+            nucGeometry.scale(0.7, 0.3, 0.7)
+            const nucSpheres = new THREE.InstancedMesh(nucGeometry, material, coordinates.length);
+            
+            //nucSpheres.scale.set(0.3, 0.7, 0.7);
+            
+            // var instancedConnector = new THREE.InstancedBufferGeometry();
+            // instancedConnector.copy(new THREE.CylinderBufferGeometry(.1,.1,1, 8) as unknown as THREE.InstancedBufferGeometry);
+
+            // var instancedBBconnector = new THREE.InstancedBufferGeometry();
+            // instancedBBconnector.copy(new THREE.CylinderBufferGeometry(.1,.02,1, 8) as unknown as THREE.InstancedBufferGeometry);
+
             const dummy = new THREE.Object3D();
+            // save dummy's default rotation
+            const dummy_rot = dummy.quaternion.clone();
     
             for (let i = 0; i < coordinates.length; i++) {
                 const {x, y, z, a1x, a1y, a1z, a3x, a3y, a3z} = coordinates[i];//.split(' ').map(parseFloat);
@@ -71,34 +76,60 @@ class oxDocument extends THREE.Group{
                     p.y + 0.4 * a1.y,
                     p.z + 0.4 * a1.z
                 )
-
-                dummy.position.set(x, y, z);
-                //dummy.position.set(ns.x, ns.y, ns.z);
-                dummy.updateMatrix();
-                spheres.setMatrixAt(i, dummy.matrix);
                 
+                //compute base rotation for the nucleoside
+                let nuc_rot = new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0, 1, 0),a3)
+                
+                
+
+                //set the bbPosition and color
+                dummy.position.set(x, y, z);
+                dummy.updateMatrix();
+                bbSpheres.setMatrixAt(i, dummy.matrix);
                 // signature oxview stand colors
-                spheres.setColorAt(i, this.oxStrandColors[strand_ids[i]%4]);
-                // random colors
-                //spheres.setColorAt(i, new THREE.Color(Math.random(), Math.random(), Math.random()));
-            }
+                bbSpheres.setColorAt(i, oxStrandColors[strand_ids[i]%4]);
+
+                //set the nucleoside position and color
+                dummy.position.set(ns.x, ns.y, ns.z);
+                // apply the nucleoside rotation
+                dummy.quaternion.copy(nuc_rot);
+                dummy.updateMatrix();
+                nucSpheres.setMatrixAt(i, dummy.matrix);
+                const cid = nucleosides_to_index[nuc_ids[i]];
+                if (cid === undefined) {
+                    //default to grey
+                    nucSpheres.setColorAt(i, nucleosideColors[nucleosideColors.length-1]);
+                }else{
+                    nucSpheres.setColorAt(i, nucleosideColors[cid]);
+                }
+                //reset dummy's rotation 
+                dummy.quaternion.copy(dummy_rot);
+                
+
+          }
           
-            this.add(spheres);
+            this.add(bbSpheres);
+            this.add(nucSpheres);
             this.app.render();
           };
 
         // fetch the topology file
         fetch(top_path).then(response => response.text())
         .then(text=>{
+            //super simple top 1.0 parser
             //parse the strand information
             let lines = text.split('\n');
             [particle_count, stand_count] = lines[0].split(' ').map(s=>s.trim()).map(parseFloat);
             console.log(particle_count, stand_count);
             strand_ids = new Array(particle_count);
+            nuc_ids = new Array(particle_count);
+
             for(let i = 1; i<particle_count+1; i++){
-                strand_ids[i-1] = lines[i].split(' ')[0];
-            }
-            //console.log(strand_id);
+                const top_line = lines[i].split(' ')
+                strand_ids[i-1] = top_line[0];
+                nuc_ids[i-1] = top_line[1];
+           }
+           
 
 
 
@@ -111,21 +142,6 @@ class oxDocument extends THREE.Group{
     }
 
     addSpheres(count = 100){
-
-        // const textureLoader = new THREE.TextureLoader();
-        // const matcap = textureLoader.load('./matcaps/03.jpg', (texture)=>{
-        //     // after loading texture, always render once
-        //     this.app.render();
-        // });
-
-        // matcap.minFilter = THREE.NearestFilter
-        // matcap.magFilter = THREE.NearestFilter
-        // matcap.generateMipmaps = false
-
-        // const material = new THREE.MeshMatcapMaterial({
-        //     matcap:matcap
-        // });
-
         material.depthTest = true
         material.depthWrite = true
         //add a ton of spheres to simulate a particle system
@@ -151,4 +167,88 @@ class oxDocument extends THREE.Group{
     }
 
 }
+
+    // Default colors for the backbones
+const oxStrandColors = [
+        new THREE.Color(0xfdd291), //light yellow
+        
+        new THREE.Color(0xffb322), //goldenrod
+
+        new THREE.Color(0x437092), //dark blue
+
+        new THREE.Color(0x6ea4cc), //light blue
+        ];
+
+
+const nucleosides_to_index = {
+    "A": 0,
+    "G": 1,
+    "C": 2,
+    "T": 3,
+    "U": 3,
+}
+
+
+const nucleosideColors = [
+    new THREE.Color(0x4747B8), //A or K; Royal Blue
+
+    new THREE.Color(0xFFFF33), //G or C; Medium Yellow
+    
+     //C or A
+        new THREE.Color(0x8CFF8C), //Medium green
+
+     //T/U or T
+        new THREE.Color(0xFF3333), //Red
+
+     //E
+        new THREE.Color(0x660000), //Dark Brown
+
+     //S
+        new THREE.Color(0xFF7042), //Medium Orange
+
+     //D
+        new THREE.Color(0xA00042), //Dark Rose
+
+     //N
+        new THREE.Color(0xFF7C70), //Light Salmon
+
+     //Q
+        new THREE.Color(0xFF4C4C), //Dark Salmon
+
+     //H
+        new THREE.Color(0x7070FF), //Medium Blue
+
+     //G
+        new THREE.Color(0xEBEBEB), // light GREY
+
+     //P
+        new THREE.Color(0x525252), //Dark Grey
+
+     //R
+        new THREE.Color(0x00007C), //Dark Blue
+
+     //V
+        new THREE.Color(0x5E005E), //Dark Purple
+
+     //I
+        new THREE.Color(0x004C00), //Dark Green
+
+     //L
+        new THREE.Color(0x455E45), //Olive Green
+
+     //M
+        new THREE.Color(0xB8A042), //Light Brown
+
+     //F
+        new THREE.Color(0x534C42), //Olive Grey
+
+     //Y
+        new THREE.Color(0x8C704C), //Medium Brown
+
+     //W
+        new THREE.Color(0x4F4600), //Olive Brown
+    //default
+        new THREE.Color(0x333333), //grey
+];
+
 export {oxDocument};
